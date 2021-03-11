6e84d5eff247f654059b3e93e5bd155421a80ff4
Vin Tang <vin@Vins-MBP.home>
2021-03-11 15:37:50 -0500

    include data viz

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from dota import main as dota_main
 import time
 from hub import transform
 import uuid
-
+import streamlit as st
 
 st.title("FastHub")
 
@@ -13,11 +13,7 @@ datasets = ['CoLA', 'Dota', '1mdb', 'mnist']
 selected_datasets = st.multiselect("Select a dataset ", datasets)
 selected_tag = st.multiselect("Tag: ", ['mynameisvinn', 'activeloop'])
 
-
-
-res = None
-
-if selected_datasets:
+if selected_datasets and selected_tag:
     dataset = selected_datasets[0]
     handle = selected_tag[0]
     random = uuid.uuid1()
@@ -35,12 +31,7 @@ if selected_datasets:
         pass
     
     if selected_datasets[0] == 'Dota':
-        url = 'https://drive.google.com/uc?id=1fwiTNqRRen09E-O9VSpcMV2e6_d4GGVK'
-        schema = {
-            'sentence': Text(shape=(None, ), max_shape=(500, )),
-            'labels': Primitive(dtype="int64")
-        }
-        res = dota_main(url)
+        pass
 
     else:
         pass
@@ -67,6 +58,12 @@ if selected_datasets and selected_tag:
     st.write("Dockerfile")
     body = f"""
         pip install hub            
-        CMD = ['python', 'fetch.py', '-user mynameisvinn']  # placeholder
-        """.format(tag)
+        CMD = ['python', 'fetch.py', '-user {handle}']  # placeholder
+        """.format(handle)
+    st.code(body, language='bash')
+
+    st.write("Easy Viz")
+    body = f"""
+    https://app.activeloop.ai/dataset/{handle}/{dataset}-{random}
+    """.format(handle, dataset, random)
     st.code(body, language='bash')
